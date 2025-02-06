3895aae8b2df0819a070e1421bc77d5c8f48a3a8
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-02-06 17:18:58 -0300

    Create 1101.c
    add

---
+++ UND1/1101.c
@@ -0,0 +1,29 @@
+//Developed by: @jtfrl
+//Beecrowd: n. 1101
+
+#include<stdio.h>
+
+int main(){
+    int a,b,i,j,t,sum=0,temp;
+    for(j=0; j<100; j++)
+    {
+      scanf("%d %d",&a,&b);
+                if(a<=0 || b<=0)
+                    break;
+                if(a>b)
+                {
+                    temp=a;
+                    a=b;
+                    b=temp;
+                }
+                for(i=a; i<=b; i++)
+                {
+                    printf("%d ",i);
+                    sum=sum+i;
+                }
+                printf("Sum=%d\n",sum);
+                sum=0;
+            }
+            return 0;
+}
+
