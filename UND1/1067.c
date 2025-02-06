3c46c6b1f219140890e5953ec37a8d27cf73f938
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-02-06 17:16:47 -0300

    Create 1067.c
    add

---
+++ UND1/1067.c
@@ -0,0 +1,18 @@
+//Developed by: @jtfrl
+//Beecrowd: n. 1067
+
+#include <stdio.h>
+int main() { int cex, cpo, X, i;
+scanf("%d", &X);
+if(X>=1&&X<=1000){
+    for(i=1;i<=X;i++){
+    if(i%2!=0){
+        printf("%d\\\\n", i);
+    }
+}
+
+}
+
+
+return 0; 
+}
